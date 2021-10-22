7a9371c3f36367acf6050dd23f8e078c4a33f531
零零狗 <48358783+kwokaa2019@users.noreply.github.com>
2021-10-22 23:49:27 +0800

    Add files via upload

---
+++ Microsoft Windows 11 Enterprise LTSC 作業系統專用.cmd
@@ -0,0 +1,68 @@
+@ECHO OFF
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM  Companyname: 
+REM  Productname: 
+REM  Filedescription: 
+REM  Copyrights: 
+REM  Trademarks: 
+REM  Originalname: 
+REM  Comments: 
+REM  Productversion:  0. 0. 0. 0
+REM  Fileversion:  0. 0. 0. 0
+REM  Internalname: 
+REM  Appicon: 
+REM  AdministratorManifest: No
+REM  QBFC Project Options End
+ECHO ON
+%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a
+cls
+@echo off
+
+echo ----------------------------------------------------------------------
+echo.
+echo                               注意事項
+echo.
+echo      此認證程式僅適用在 Microsoft Windows 11 Enterprise LTSC  作業系統，不適用其他作業系統。
+echo.
+echo      This certifi program is only applicable to Microsoft Windows 11 Enterprise LTSC operating system, not to other operating systems.。
+echo.
+echo ----------------------------------------------------------------------
+
+echo Windows 認證中...
+
+echo 請耐心等候...
+
+echo.
+
+if exist %systemroot%\system32\slmgr.vbs GOTO start
+
+GOTO fail
+
+:start
+
+cd\
+
+cscript %systemroot%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+
+cscript %systemroot%\system32\slmgr.vbs -skms 192.168.1.100:1688 > %temp%\qwdv.tmp
+
+if exist %temp%\qwdv.tmp del %temp%\qwdv.tmp
+
+cscript %systemroot%\system32\slmgr.vbs -ato > %temp%\qwdv.tmp
+
+if exist %temp%\sdfw.tmp del %temp%\sdfw.tmp
+
+cscript %systemroot%\system32\slmgr.vbs -xpr
+
+goto end
+
+:fail
+
+echo Windows 認證失敗，請確認作業系統版本或電腦網路狀態。
+
+echo.
+
+:end
+
+pause
